9f7e4b167efd54843caf7f9446bcdfa6917fcb87
ElMan23 <elia.manara@yahoo.it>
2023-09-18 17:31:37 +0200

    crud operations with stub

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import random
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+import random
 
 app = FastAPI()
 
@@ -12,12 +14,63 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+post_list = [
+    {
+        "id": 1,
+        "title": "My first post",
+        "content": "This is the content",
+        "published": False,
+        "rating": 3
+    },
+    {
+        "id": 2,
+        "title": "My first post",
+        "content": "This is the content",
+        "published": False,
+        "rating": 3
+    }
+]
+
+
 @app.get("/posts")
 async def get_posts():
-    return {"data": "This is your post!"}
+    return {"data": post_list}
 
 
-@app.post("/createpost")
-def create_posts(post: Post):
-    print(post.model_dump())
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = random.randint(1, 1000)
+    post_list.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = _find_post(int(id))
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with ID {id} was not found.")
     return {"data": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id):
+    idx = _find_index_post(int(id))
+    if not idx:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with ID {id} was not found.")
+    post_list.pop(int(idx))
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+def _find_index_post(id: int):
+    for i, p in enumerate(post_list):
+        if p["id"] == id:
+            return i
+    return None
+
+
+def _find_post(id: int):
+    for p in post_list:
+        if p["id"] == id:
+            return p
+    return None
