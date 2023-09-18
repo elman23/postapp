9e840a91f498ac536843ae788409c372b38b8d6e
ElMan23 <elia.manara@yahoo.it>
2023-09-18 16:03:56 +0200

    add new post

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "This is your post!"}
+
+
+@app.post("/createpost")
+def create_posts(post: Post):
+    print(post)
+    return {"data": "new post"}
