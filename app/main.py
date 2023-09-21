fb7e008e736c8796a34bf65e6a1b56a4fa94a3a3
ElMan23 <elia.manara@yahoo.it>
2023-09-21 09:04:36 +0200

    cors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
-from . import models, auth
-from .database import engine
+from . import auth
 from .routers import post, user, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # Auto generate database
 # models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+
+origins = ["*"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 app.include_router(auth.router)
