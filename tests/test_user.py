6c20478d1b9f30a9c7e4a0cad04994c943b7da1e
ElMan23 <elia.manara@yahoo.it>
2023-09-25 15:35:56 +0200

    generating tables at test start

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,6 +6,7 @@ from app.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.database import get_db, Base
+import pytest
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
@@ -13,7 +14,6 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine) # Generate database tables
 
 
 def override_get_db():
@@ -26,17 +26,26 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
-client = TestClient(app)
+
+@pytest.fixture
+def client():
+    # Run code before test run
+    Base.metadata.drop_all(bind=engine)  # Drop database tables
+    Base.metadata.create_all(bind=engine)  # Generate database tables
+    # Yield client
+    yield TestClient(app)
+    # Run code after test finishes
+    # Base.metadata.drop_all(bind=engine)  # Drop database tables
 
 
-def test_root():
+def test_root(client):
     response = client.get("/")
     print("Testing root path of application.")
     assert response.json().get("message") == "Post API"
     assert response.status_code == status.HTTP_200_OK
 
 
-def test_create_user():
+def test_create_user(client):
     response = client.post("/users", json={"email": "test@email.com", "password": "password"})
     new_user = schemas.ResponseUser(**response.json())
     assert new_user.email == "test@email.com"
