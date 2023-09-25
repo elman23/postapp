23c4e6991ad46eda5c6cf94434661e2083ad3f1d
ElMan23 <elia.manara@yahoo.it>
2023-09-25 12:20:59 +0200

    parametrizing test

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,21 +1,46 @@
 from app.calculations import add, sub, mult, quot
+import pytest
 
 
-def test_add():
+@pytest.mark.parametrize("n1, n2, expected", [
+    (1, 2, 3),
+    (3, 2, 5),
+    (7, 1, 8),
+    (12, 4, 16)
+])
+def test_add(n1, n2, expected):
     print("Testing add...")
-    assert add(1, 2) == 3
+    assert add(n1, n2) == expected
 
 
-def test_sub():
+@pytest.mark.parametrize("n1, n2, expected", [
+    (2, 1, 1),
+    (5, 2, 3),
+    (7, 1, 6),
+    (12, 4, 8)
+])
+def test_sub(n1, n2, expected):
     print("Testing sub...")
-    assert sub(3, 1) == 2
+    assert sub(n1, n2) == expected
 
 
-def test_mult():
+@pytest.mark.parametrize("n1, n2, expected", [
+    (1, 2, 2),
+    (3, 2, 6),
+    (7, 1, 7),
+    (12, 4, 48)
+])
+def test_mult(n1, n2, expected):
     print("Testing mult...")
-    assert mult(3, 2) == 6
+    assert mult(n1, n2) == expected
 
 
-def test_div():
+@pytest.mark.parametrize("n1, n2, expected", [
+    (10, 2, 5),
+    (30, 3, 10),
+    (15, 5, 3),
+    (12, 4, 3)
+])
+def test_div(n1, n2, expected):
     print("Testing quot...")
-    assert quot(10, 2) == 5
+    assert quot(n1, n2) == expected
