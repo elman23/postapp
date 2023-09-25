6e1b3086143fccc05edb1ef532e086bc11f7484a
ElMan23 <elia.manara@yahoo.it>
2023-09-25 15:28:26 +0200

    creating test database

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,7 +2,29 @@ from fastapi.testclient import TestClient
 from fastapi import status
 from app.main import app
 from app import schemas
+from app.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database import get_db, Base
 
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine) # Generate database tables
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
