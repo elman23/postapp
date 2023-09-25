15d0fa631783a04eaac337db525e1eaa845735c8
ElMan23 <elia.manara@yahoo.it>
2023-09-25 12:38:12 +0200

    adding pytest fixtures

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -2,6 +2,18 @@ from app.calculations import add, sub, mult, quot, BankAccount
 import pytest
 
 
+@pytest.fixture
+def zero_bank_account():
+    print("Creating zero-balance bank account.")
+    return BankAccount()
+
+
+@pytest.fixture
+def bank_account():
+    print("Creating 50 balance bank account.")
+    return BankAccount(50)
+
+
 @pytest.mark.parametrize("n1, n2, expected", [
     (1, 2, 3),
     (3, 2, 5),
@@ -46,29 +58,29 @@ def test_div(n1, n2, expected):
     assert quot(n1, n2) == expected
 
 
-def test_bank_set_initial_amount():
-    bank_account = BankAccount(50)
+def test_bank_set_default_amount(zero_bank_account):
+    print("Testing default amount for BankAccount.")
+    assert zero_bank_account.balance == 0
+
+
+def test_bank_set_initial_amount(bank_account):
+    print("Testing initial amount for BankAccount.")
     assert bank_account.balance == 50
 
 
-def test_bank_set_default_amount():
-    bank_account = BankAccount()
-    assert bank_account.balance == 0
-
-
-def test_withdraw():
-    bank_account = BankAccount(50)
+def test_withdraw(bank_account):
+    print("Testing withdraw for BankAccount.")
     bank_account.withdraw(20)
     assert bank_account.balance == 30
 
 
-def test_deposit():
-    bank_account = BankAccount(50)
+def test_deposit(bank_account):
+    print("Testing deposit for BankAccount.")
     bank_account.deposit(30)
     assert bank_account.balance == 80
 
 
-def test_collect_interest():
-    bank_account = BankAccount(50)
+def test_collect_interest(bank_account):
+    print("Testing collect interest for BankAccount.")
     bank_account.collect_interest()
     assert bank_account.balance == 55
