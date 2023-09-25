57b81091c6f202676689cdc2880bd251a3d50166
ElMan23 <elia.manara@yahoo.it>
2023-09-25 15:17:15 +0200

    testing user creation

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
+from fastapi import status
 from app.main import app
+from app import schemas
 
 
 client = TestClient(app)
@@ -9,4 +11,11 @@ def test_root():
     response = client.get("/")
     print("Testing root path of application.")
     assert response.json().get("message") == "Post API"
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_create_user():
+    response = client.post("/users", json={"email": "test@email.com", "password": "password"})
+    new_user = schemas.ResponseUser(**response.json())
+    assert new_user.email == "test@email.com"
+    assert response.status_code == status.HTTP_201_CREATED
