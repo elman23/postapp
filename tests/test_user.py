debd4f2b50bcfffd6c0c20c50bf649945bf553be
ElMan23 <elia.manara@yahoo.it>
2023-09-26 09:58:33 +0200

    completed login user test

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,9 @@
 import pytest
+from jose import jwt
 from fastapi import status
 from app import schemas
 from .database import client, session
-import pytest
+from app.config import settings
 
 
 @pytest.fixture
@@ -35,3 +36,8 @@ def test_login_user(client, test_user):
     print("Testing user login...")
     response = client.post("/login", data={"username": test_user["email"], "password": test_user["password"]})
     assert response.status_code == status.HTTP_200_OK
+    login_response = schemas.Token(**response.json())
+    payload = jwt.decode(login_response.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_response.token_type == "bearer"
