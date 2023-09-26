e978d9b6efb5fdf4374c9b781cc9c21475895337
ElMan23 <elia.manara@yahoo.it>
2023-09-26 09:41:54 +0200

    fixture scope

--- tests/database.py
+++ tests/database.py
@@ -14,7 +14,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def session():
     Base.metadata.drop_all(bind=engine)  # Drop database tables
     Base.metadata.create_all(bind=engine)  # Generate database tables
@@ -25,7 +25,7 @@ def session():
         db.close()
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def client(session):
     def override_get_db():
         try:
--- tests/test_user.py
+++ tests/test_user.py
@@ -4,14 +4,21 @@ from .database import client, session
 
 
 def test_root(client):
+    print("Testing root path of application...")
     response = client.get("/")
-    print("Testing root path of application.")
     assert response.json().get("message") == "Post API"
     assert response.status_code == status.HTTP_200_OK
 
 
 def test_create_user(client):
+    print("Testing user creation...")
     response = client.post("/users", json={"email": "test@email.com", "password": "password"})
     new_user = schemas.ResponseUser(**response.json())
     assert new_user.email == "test@email.com"
     assert response.status_code == status.HTTP_201_CREATED
+
+
+def test_login_user(client):
+    print("Testing user login...")
+    response = client.post("/login", data={"username": "test@email.com", "password": "password"})
+    assert response.status_code == status.HTTP_200_OK
