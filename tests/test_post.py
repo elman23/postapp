6a2bd153cabd6a4f16741be15844a426d82393a3
ElMan23 <elia.manara@yahoo.it>
2023-09-26 10:35:19 +0200

    returned post schema validation

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,8 +1,11 @@
 from fastapi import status
+from app import schemas
 
 
 def test_get_all_post(authorized_client, test_posts):
     print("Testing get all posts...")
     response = authorized_client.get("/posts")
-    print(response.json())
+    posts = [schemas.ResponsePostVote(**post) for post in response.json()]
+    assert posts
+    assert len(posts) == len(test_posts)
     assert response.status_code == status.HTTP_200_OK
