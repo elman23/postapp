5bdfeaa2f29043ab4eaf249107abb07692606636
ElMan23 <elia.manara@yahoo.it>
2023-09-26 10:42:04 +0200

    get all posts unauthorized

--- tests/test_post.py
+++ tests/test_post.py
@@ -9,3 +9,9 @@ def test_get_all_post(authorized_client, test_posts):
     assert posts
     assert len(posts) == len(test_posts)
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    print("Testing unauthorized client getting all posts...")
+    response = client.get("/posts")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
