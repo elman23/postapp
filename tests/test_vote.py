19e883bdcc9574542f05ed0d855fdce55d2b3a11
ElMan23 <elia.manara@yahoo.it>
2023-09-26 12:18:13 +0200

    more vote tests

--- tests/test_vote.py
+++ tests/test_vote.py
@@ -28,3 +28,14 @@ def test_vote_delete_post(authorized_client, test_posts, test_vote):
 def test_vote_down_post(authorized_client, test_posts):
     response = authorized_client.post("/vote", json={"post_id": test_posts[0].id, "direction": 0})
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_vote_not_existent_post(authorized_client, test_posts):
+    not_existent_id = 1000
+    response = authorized_client.post("/vote", json={"post_id": not_existent_id, "direction": 1})
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_vote_unauthorized_user(client, test_posts):
+    response = client.post("/vote", json={"post_id": test_posts[0].id, "direction": 1})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
